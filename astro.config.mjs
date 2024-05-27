f415fefc05397a0e7bb867fce6e59cd3e4cddc54
Gilbert Luis <gilbertluis97@gmail.com>
2024-05-27 17:06:03 -0400

    experimentfanod para ahcer deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
 
-	site: 'https://gilbertL97.github.io/thecoveatx-2.0',
-	
+	site: 'https://gilbertL97.github.io',
+	base: 'https://gilbertL97.github.io/thecoveatx-2.0',
 	integrations: [tailwind()],
 	renderers: ['@astrojs/renderer-github-pages'],
 	i18n: {
