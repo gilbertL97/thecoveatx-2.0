a81fb5b8c38e302534adce79dff186fb9a375a52
Gilbert Luis <gilbertluis97@gmail.com>
2024-05-27 14:44:28 -0400

    agregando action

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+
+	site: 'https://gilbertL97.github.io',
 	integrations: [tailwind()],
+	renderers: ['@astrojs/renderer-github-pages'],
 	i18n: {
 		defaultLocale: 'en',
 		locales: ['es', 'en']
